f3c1417615133dda628ab463dbe8ab27cdfebc17
lvyahui8 <lvyahui8@126.com>
2019-07-02 23:25:39 +0800

    Optimized code

--- spring-boot-data-aggregator-core/src/main/java/io/github/lvyahui8/spring/aggregate/model/DependType.java
+++ spring-boot-data-aggregator-core/src/main/java/io/github/lvyahui8/spring/aggregate/model/DependType.java
@@ -1,10 +1,18 @@
 package io.github.lvyahui8.spring.aggregate.model;
 
 /**
+ * The dependency type of the parameter of the data provider's method
+ *
  * @author lvyahui (lvyahui8@gmail.com,lvyahui8@126.com)
  * @since 2019/6/3 22:44
  */
 public enum DependType {
+    /**
+     * Caller passed parameters
+     */
     INVOKE_PARAM,
+    /**
+     * Parameters that require automatic injection
+     */
     OTHER_MODEL
 }
