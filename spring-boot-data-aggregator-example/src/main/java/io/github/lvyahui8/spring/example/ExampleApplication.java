4c7462565a30df01250501e9cabb425dadb6d55c
lvyahui8 <lvyahui8@126.com>
2019-08-05 22:18:43 +0800

    update version && travis-ci

--- spring-boot-data-aggregator-example/src/main/java/io/github/lvyahui8/spring/example/ExampleApplication.java
+++ spring-boot-data-aggregator-example/src/main/java/io/github/lvyahui8/spring/example/ExampleApplication.java
@@ -1,15 +1,10 @@
 package io.github.lvyahui8.spring.example;
 
-import io.github.lvyahui8.spring.aggregate.facade.DataBeanAggregateQueryFacade;
-import io.github.lvyahui8.spring.example.facade.UserQueryFacade;
-import io.github.lvyahui8.spring.example.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.util.Assert;
 
-import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -21,39 +16,16 @@ import java.util.concurrent.ExecutorService;
 @Slf4j
 public class ExampleApplication {
 
-    private static final int NUM = 100;
-
     public static void main(String[] args) throws Exception {
-        ConfigurableApplicationContext context = SpringApplication.run(ExampleApplication.class);
+        ConfigurableApplicationContext context = null;
         try{
-            DataBeanAggregateQueryFacade queryFacade = context.getBean(DataBeanAggregateQueryFacade.class);
-
-            {
-                User user = queryFacade.get("userWithPosts", Collections.singletonMap("userId",1L), User.class);
-                Assert.notNull(user,"user not null");
-                Assert.notNull(user.getPosts(),"user posts not null");
-                log.info("user.name:{},user.posts.size:{}",
-                        user.getUsername(),user.getPosts().size());
-            }
-
-            log.info("------------------------------------------------------------------");
-
-            {
-                User user = context.getBean(UserQueryFacade.class).getUserFinal(1L);
-                Assert.notNull(user.getFollowers(),"user followers not null");
-            }
-
-            log.info("------------------------------------------------------------------");
-
-            {
-                for (int i = 0; i < NUM; i ++) {
-                    String s = queryFacade.get("categoryTitle", Collections.singletonMap("categoryId", 1L), String.class);
-                    Assert.isTrue(org.apache.commons.lang3.StringUtils.isNotEmpty(s),"s  not null");
-                }
-            }
+            context =  SpringApplication.run(ExampleApplication.class);
         } finally {
-            ExecutorService executorService = (ExecutorService) context.getBean("aggregateExecutorService");
-            executorService.shutdown();
+            if(context != null) {
+                ExecutorService executorService = (ExecutorService) context.getBean("aggregateExecutorService");
+                executorService.shutdown();
+            }
         }
     }
+
 }
