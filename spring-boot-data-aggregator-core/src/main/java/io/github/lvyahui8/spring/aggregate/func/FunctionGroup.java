950cbe65e0ad83e6237acdb2668afc29858eb76d
feego <lvyahui8@gmail.com>
2022-02-05 21:36:25 +0800

    group invoke

--- spring-boot-data-aggregator-core/src/main/java/io/github/lvyahui8/spring/aggregate/func/FunctionGroup.java
+++ spring-boot-data-aggregator-core/src/main/java/io/github/lvyahui8/spring/aggregate/func/FunctionGroup.java
@@ -3,6 +3,9 @@ package io.github.lvyahui8.spring.aggregate.func;
 import io.github.lvyahui8.spring.aggregate.context.AggregationContext;
 
 /**
+ *
+ * 一组类实现此接口，并注解上groupKey以并发调用
+ *
  * @author feego lvyahui8@gmail.com
  * @date 2022/2/5
  */
